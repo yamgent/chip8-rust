c5e8a5daafac7b62597af14219a55ae8a6b53cd2
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-01-11 21:21:38 +0800

    Add blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn run() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .expect("Failed to build window");
+
+    event_loop.run(move |event, _, control_flow| {
+        // it is ok to wait until we have the next re-draw, no need to keep
+        // spinning
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            _ => {}
+        }
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    run();
 }
